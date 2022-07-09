be21976008a9a62d9f6eeb256ddcb4d9ce030cba
Yoon Hwa Pyeong <dkwjt001@korea.ac.kr>
2022-07-09 21:10:44 +0900

    init

--- 노마더코드/ReactJS로 영화 웹 서비스 만들기/react-for-beginners/src/routes/Home.js
+++ 노마더코드/ReactJS로 영화 웹 서비스 만들기/react-for-beginners/src/routes/Home.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
@@ -25,6 +26,7 @@ function Home() {
           {movies.map((movie) => (
             <Movie
               key={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
